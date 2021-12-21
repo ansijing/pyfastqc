38e76e12d6682c152643c31926392d872739390e
zhen-he <hezhen.cs@gmail.com>
2021-12-21 17:03:59 +0800

    deleted:    src/SRR1976948_1.fastq 	modified:   src/readfq.py

--- src/readfq.py
+++ src/readfq.py
@@ -1,7 +1,10 @@
 import gzip
-file_read = gzip.open("/root/data/asj/pyfastqc/data/SRR1976948_1.fastq.gz", 'rb')
-file_out=open("SRR1976948_1.fastq","w")   
-file_content = file_read.readlines() 
-line_counts= len(file_content)
-line_out = int(line_counts / 4)
-print(line_out)    
+def readfq(file_read):
+    """Total Sequences"""
+    file_read_1 = gzip.open(file_read)  
+    file_content = file_read_1.readlines() 
+    line_counts= len(file_content)
+    line_out = int(line_counts / 4)
+    print(line_out)
+readfq("/root/data/asj/pyfastqc/data/SRR1976948_1.fastq.gz")
+        
