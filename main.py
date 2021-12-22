a3a5e6baa3b952ad7f2a6f1aecd0ca8fb32abc4e
ansijing <1239484619@qq.com>
2021-12-22 16:58:36 +0800

    estimate the type of phred

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ if __name__ == '__main__':
     #step2. the sequence length 
     len_max, len_min = readfq.get_fq_sequence_lengh('data/test.fastq')
     print('sequence length: ', len_max, len_min )
-    
-    
+    # step3. estimate the type of phred
+    qual_type= readfq.get_enc_type('data/test.fastq')
+    print('sequence length: ', qual_type)
--- src/readfq.py
+++ src/readfq.py
@@ -50,5 +50,39 @@ def get_fq_sequence_lengh(file_path):
     seq_len_min = min(seq_len_list)
     print("seqmax:", seq_len_max)    
         
-    return seq_len_min, seq_len_max           
- 
+    return seq_len_min, seq_len_max  
+         
+
+def get_enc_type(file_path): 
+    """by dealing Base quality score, get the type of Phred qualitiy """
+    if not os.path.exists(file_path):
+        print('file not exists! Please check')
+        return
+    print('Input file: ', file_path)
+    
+    if file_path.endswith('.gz'):
+        my_open = gzip.open
+    else:
+        my_open = open
+        
+    quali_score_list = []
+    with my_open(file_path,'r') as inpf:
+        i = -1 
+        for line in inpf:
+            line = str(line.strip())
+            #print('in loop: line is ', line)
+            i = i + 1
+            #print(' i is: ', i)
+            if i%4 == 3:
+                #print('enter i%4==3, line: ',line)
+                for score in line:
+                    quali_score = ord(score)
+                    quali_score_list.append(quali_score)
+        
+    quali_score_max = max(quali_score_list)
+    if quali_score_max <74:
+        Phred_type = 'phred_33'
+    else:
+        Phred_type = 'phred_64'
+    return Phred_type
+        
